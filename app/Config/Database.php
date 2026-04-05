e178130245504bc830d01d0248b891d1fcacce46
Danith K Pradeep <kpkumara101@gmail.com>
2026-04-05 16:35:08 +0530

    Setup Docker with MySQL and phpMyAdmin

--- app/Config/Database.php
+++ app/Config/Database.php
@@ -26,9 +26,9 @@ class Database extends Config
      */
     public array $default = [
         'DSN'          => '',
-        'hostname'     => 'localhost',
+        'hostname'     => 'db',             
         'username'     => 'root',
-        'password'     => '',
+        'password'     => 'root_password',  
         'database'     => 'autocare',
         'DBDriver'     => 'MySQLi',
         'DBPrefix'     => '',
---
+++ app/Database/Migrations/2026-04-05-000002_CreateStationsTable.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Database\Migrations;
+
+use CodeIgniter\Database\Migration;
+
+class CreateStationsTable extends Migration
+{
+    public function up()
+    {
+        //
+    }
+
+    public function down()
+    {
+        //
+    }
+}
